30446c5d51bc0d5ba5f7260c022dc9c824a3b0fb
Huanle01 <huanle120201@gmail.com>
2023-12-27 22:50:54 +0700

    Organizations

---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./_component/Navbar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+export default DashboardLayout;
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,6 @@
+const OrganizationIdPage =()=>{
+    return (
+        <div>Organization Page</div>
+    )
+}
+export default OrganizationIdPage
