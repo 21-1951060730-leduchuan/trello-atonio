01f641f5f6fb5721e0656b69990cc5886cd20453
Huanle01 <huanle120201@gmail.com>
2024-01-01 02:32:27 +0700

    Board list

--- app/(platform)/(dashboard)/organization/[organizationId]/_component/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_component/board-list.tsx
@@ -1,29 +1,68 @@
 import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import { HelpCircle, User2 } from "lucide-react";
-export const BoardList = () => {
+import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
+export const BoardList = async () => {
+  const { orgId } = auth();
+  if (!orgId) {
+    return redirect("/select-org");
+  }
+  const boards = await db.board.findMany({
+    where: { orgId },
+    orderBy: { createdAt: "desc" },
+  });
   return (
     <div className="space-x-4">
       <div className="flex items-center font-semibold text-lg text-neutral-700">
         <User2 className="h-6 w-6 mr-2" /> Your Boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        <FormPopover sideOffset={10} side='right'>
-        <div
-          role="button"
-          className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-        >
-            <p className="text-sm">
-                Create New Board
-            </p>
-            <span className="text-xs">5 Remaining
-            </span>
-            <Hint sideOffset={40} description={`Free Workspaces can have up to 5 open boards.Form Limited boards upgrade this workspace.`}> 
-                <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]"/>
+        {boards.map((board) => (
+          <Link
+            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+            key={board.id}
+            href={`/board/${board.id}`}
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+          >
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition">
+                <p className="relative font-semibold text-white">{board.title}</p>
+            </div>
+          </Link>
+        ))}
+        <FormPopover sideOffset={10} side="right">
+          <div
+            role="button"
+            className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+          >
+            <p className="text-sm">Create New Board</p>
+            <span className="text-xs">5 Remaining</span>
+            <Hint
+              sideOffset={40}
+              description={`Free Workspaces can have up to 5 open boards.Form Limited boards upgrade this workspace.`}
+            >
+              <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
             </Hint>
-        </div>
+          </div>
         </FormPopover>
       </div>
     </div>
   );
 };
+BoardList.Skeleton = function SkeletonBoardList(){
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+      <Skeleton className='aspect-video h-full w-full p-2'/>
+    </div>
+  )
+}
