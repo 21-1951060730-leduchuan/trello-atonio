304e22d2003cb54bf26d1674fdf67f9f8b9bf287
Huanle01 <huanle120201@gmail.com>
2024-01-12 16:17:36 +0700

    drag and drop

--- actions/update-card-order/schema.ts
+++ actions/update-card-order/schema.ts
@@ -1,9 +1,15 @@
 import { z } from "zod";
+
 export const UpdateCardOrder = z.object({
-  items: z
-    .array(z.object({ id: z.string(),listId:z.string(), title: z.string() ,order: z.number(),createdAt: z.date(),updatedAt: z.date()}))
-    .min(3, { message: "title is too short" }),
-
-
+  items: z.array(
+    z.object({
+      id: z.string(),
+      title: z.string(),
+      order: z.number(),
+      listId: z.string(),
+      createdAt: z.date(),
+      updatedAt: z.date(),
+    }),
+  ),
   boardId: z.string(),
 });
